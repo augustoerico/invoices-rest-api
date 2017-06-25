10677008ea023de510396d85ca6ff08cdf843fba
Erico Silva <augusto.ericosilva@gmail.com>
2017-06-25 14:12:34 -0300

    Add json test

--- src/test/scala/io/github/augustoerico/models/InvoiceUnitSpec.scala
+++ src/test/scala/io/github/augustoerico/models/InvoiceUnitSpec.scala
@@ -1,11 +1,13 @@
 package io.github.augustoerico.models
 
+import io.vertx.core.json.JsonObject
 import org.scalatest.{FlatSpec, Matchers}
 
 class InvoiceUnitSpec extends FlatSpec with Matchers {
 
   "An invoice" should "be created with given parameters" in {
     val invoice = new Invoice("1", "2", "type", 1.0, 0)
+
     invoice.customerId should be ("1")
     invoice.addressId should be ("2")
     invoice._type should be ("type")
@@ -15,6 +17,7 @@ class InvoiceUnitSpec extends FlatSpec with Matchers {
 
   "An invoice" should "be created with default parameters" in {
     val invoice = new Invoice("1", "2")
+
     invoice.customerId should be ("1")
     invoice.addressId should be ("2")
     invoice._type should be ("shop")
@@ -36,6 +39,21 @@ class InvoiceUnitSpec extends FlatSpec with Matchers {
 
   "An exception" should "be throw for invalid creation time" in {
     a [IllegalArgumentException] should be thrownBy { new Invoice("1", "2", "type", 1.0, -10L) }
+  }
+
+  "An invoice" should "be created from a valid JSON" in {
+    val json = new JsonObject()
+      .put("customerId", "1")
+      .put("addressId", "2")
+      .put("type", "type")
+      .put("amount", 10.0)
+    val invoice = new Invoice(json)
+
+    invoice.customerId should be ("1")
+    invoice.addressId should be ("2")
+    invoice._type should be ("type")
+    invoice.amount should be (10.0)
+    invoice.createdAt should be >= 0L
   }
 
 }
