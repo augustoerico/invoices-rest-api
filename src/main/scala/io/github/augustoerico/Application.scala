1794950d6071ab2e530ae413f49c9ae0f440f00c
Erico Silva <augusto.ericosilva@gmail.com>
2017-06-25 12:25:47 -0300

    Refactor list handler

--- src/main/scala/io/github/augustoerico/Application.scala
+++ src/main/scala/io/github/augustoerico/Application.scala
@@ -3,9 +3,9 @@ package io.github.augustoerico
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
+import io.github.augustoerico.handlers.ListHandler
 import io.github.augustoerico.repository.Repository
 import io.vertx.core.json.JsonArray
-
 import io.vertx.core.{AsyncResult, Vertx}
 import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.{Router, RoutingContext}
@@ -34,27 +34,7 @@ object Application extends App {
       context.response().setStatusCode(200).end(data.encodePrettily())
     })
 
-    router.get("/v1/customers/:customerId/invoices").handler((context: RoutingContext) => {
-      println(s"[${context.request().method()}] ${context.request().absoluteURI()}")
-
-      val request = context.request()
-      val query = new JsonObject()
-        .put("customerId", request.getParam("customerId"))
-        .put("month", request.getParam("month"))
-        .put("filter", request.getParam("filter"))
-
-      new Repository(context.vertx()).find("invoices", query, (future: AsyncResult[java.util.List[JsonObject]]) => {
-        val response = context.response()
-
-        if (future.succeeded()) {
-          val data = new JsonArray(future.result()).encodePrettily()
-          response.setStatusCode(200).end(data)
-        } else {
-          future.cause().printStackTrace()
-        }
-
-      })
-    })
+    router.get("/v1/customers/:customerId/invoices").handler(new ListHandler().handle _)
 
     router.get("/v1/customers/:customerId/addresses/:addressId/invoices").handler((context: RoutingContext) => {
       println(s"[${context.request().method()}] ${context.request().absoluteURI()}")
---
+++ src/main/scala/io/github/augustoerico/handlers/ListHandler.scala
@@ -0,0 +1,33 @@
+package io.github.augustoerico.handlers
+
+import io.github.augustoerico.repository.Repository
+import io.vertx.core.{AsyncResult, Handler}
+import io.vertx.core.json.JsonArray
+import io.vertx.ext.web.RoutingContext
+import io.vertx.lang.scala.json.JsonObject
+
+class ListHandler extends Handler[RoutingContext] {
+
+  override def handle(context: RoutingContext): Unit = {
+    println(s"[${context.request().method()}] ${context.request().absoluteURI()}")
+
+    val request = context.request()
+    val query = new JsonObject()
+      .put("customerId", request.getParam("customerId"))
+      .put("month", request.getParam("month"))
+      .put("filter", request.getParam("filter"))
+
+    new Repository(context.vertx()).find("invoices", query, (future: AsyncResult[java.util.List[JsonObject]]) => {
+      val response = context.response()
+
+      if (future.succeeded()) {
+        val data = new JsonArray(future.result()).encodePrettily()
+        response.setStatusCode(200).end(data)
+      } else {
+        future.cause().printStackTrace()
+      }
+
+    })
+  }
+
+}
